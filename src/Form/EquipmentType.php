901e121e95239a1f97dc17b79ce9b7231992fada
Oussama Fekih Hassen <hssanoussama@gmail.com>
2023-12-21 22:10:55 +0100

    design the rest of forms

--- src/Form/EquipmentType.php
+++ src/Form/EquipmentType.php
@@ -4,21 +4,31 @@ namespace App\Form;
 
 use App\Entity\Equipment;
 use App\Entity\ProjectDeRecherche;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class EquipmentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('etat')
+            ->add('nom',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
+            ->add('etat',null,[
+                'label'=>'disponible?','attr'=>[
+                    'class'=>'box'
+                ]
+            ])
             ->add('projectDeRecherche', EntityType::class, [
                 'class' => ProjectDeRecherche::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
             ])
         ;
     }
--- src/Form/PublicationType.php
+++ src/Form/PublicationType.php
@@ -2,24 +2,35 @@
 
 namespace App\Form;
 
-use App\Entity\ProjectDeRecherche;
 use App\Entity\Publication;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\ProjectDeRecherche;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PublicationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
+            ->add('titre',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
             ->add('auteurs')
-            ->add('motsCles')
+            ->add('motsCles',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
             ->add('projectDeRecherche', EntityType::class, [
                 'class' => ProjectDeRecherche::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
             ])
         ;
     }
