9be8dacba391335057f8c344fed25ad17a118b2e
Oussama Fekih Hassen <hssanoussama@gmail.com>
2023-12-21 21:33:48 +0100

    design addproject

--- src/Form/ProjectDeRechercheType.php
+++ src/Form/ProjectDeRechercheType.php
@@ -4,22 +4,40 @@ namespace App\Form;
 
 use App\Entity\Chercheur;
 use App\Entity\ProjectDeRecherche;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class ProjectDeRechercheType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('description')
-            ->add('etatAvancement')
+            ->add('titre',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
+            ->add('description',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
+            ->add('etatAvancement',null,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
             ->add('chercheur', EntityType::class, [
                 'class' => Chercheur::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
             ])
         ;
     }
