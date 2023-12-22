d5701347e4c98f1ba4147a413564859722e14ed0
Oussama Fekih Hassen <hssanoussama@gmail.com>
2023-12-22 23:23:59 +0100

    update

--- src/Form/ChercheurType.php
+++ src/Form/ChercheurType.php
@@ -6,17 +6,43 @@ use App\Entity\Chercheur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ChercheurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('motDePasse')
-            ->add('role')
+            ->add('nom',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
+            ->add('prenom',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
+            ->add('email',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
+            ->add('motDePasse',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
+            ->add('role',TextType::class,[
+                'attr'=>[
+                'class' =>'form-control inp',
+                'id'=>'searchPublication',
+                ]
+            ])
         ;
     }
 
